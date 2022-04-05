dc138cbf00214674d173a138fb7d0f5d771a2155
scoxen1 <sean.coxen@gmail.com>
2022-04-04 20:56:29 -0400

    fixes to attribute entity slideout - data type, eng unit on change not binding properly

--- frontend/src/services/AttributesService.js
+++ frontend/src/services/AttributesService.js
@@ -246,6 +246,7 @@ export const onChangeEngUnitShared = (val, item, lookupEngUnits) => {
 
     //set engunit - can be null (if they select "-1" value)
     item.engUnit = match;
+    item.engUnitId = match.id;
 
     return item;
 };
@@ -413,7 +414,7 @@ export const renderEngUnitUIShared = (editItem, lookupEngUnits, onChangeCallback
     //map value bind to structure the control accepts
     var selValue = {
         label: editItem.engUnit?.id == null || editItem.engUnit.id.toString() === "-1" ?
-            "Select" : editItem.engUnit.name,
+            "Select" : editItem.engUnit.displayName,
         value: editItem.engUnit?.id == null ? -1 : editItem.engUnit.id
     };
 

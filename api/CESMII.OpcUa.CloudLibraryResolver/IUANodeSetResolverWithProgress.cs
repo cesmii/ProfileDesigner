11c66a8c9edfcc2482e7d186f31fac050217eddc
Markus Horstmann <markushorstmann@hotmail.com>
2022-05-26 18:28:52 -0700

    CloudLib download: user progress reporting, use new GetNodeSetDependencies API.

---
+++ api/CESMII.OpcUa.CloudLibraryResolver/IUANodeSetResolverWithProgress.cs
@@ -0,0 +1,22 @@
+﻿/* Author:      Markus Horstmann, C-Labs
+ * Last Update: 5/26/2022
+ * License:     MIT
+ * 
+ * Some contributions thanks to CESMII – the Smart Manufacturing Institute, 2022
+ */
+
+using System;
+
+namespace CESMII.OpcUa.NodeSetImporter
+{
+    public interface IUANodeSetResolverWithProgress : IUANodeSetResolver
+    {
+        public OnResolveNodeSets OnResolveNodeSets { get; set; }
+        public OnNodeSet OnDownloadNodeSet { get; set; }
+        public OnNodeSet OnNodeSetFound { get; set; }
+        public OnNodeSet OnNodeSetNotFound { get; set; }
+    }
+    public delegate void OnResolveNodeSets();
+    public delegate void OnNodeSet(string namespaceUri, DateTime? publicationDate);
+
+}

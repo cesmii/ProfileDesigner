da60a92c61ab841c4125f13d52f94cc431429573
Markus Horstmann <markushorstmann@hotmail.com>
2022-06-13 19:14:05 -0700

    DAL: revert change for query order

--- api/CESMII.ProfileDesigner.DAL/TenantBaseDAL.cs
+++ api/CESMII.ProfileDesigner.DAL/TenantBaseDAL.cs
@@ -51,7 +51,7 @@
         protected override DALResult<TModel> Where(Expression<Func<TEntity, bool>> predicate, UserToken user, int? skip = null, int? take = null, bool returnCount = false, bool verbose = false,
             Func<IQueryable<TEntity>, IQueryable<TEntity>> additionalQuery = null)
         {
-            var query = _repo.FindByCondition(e => e.OwnerId == null || e.OwnerId == user.UserId).Where(predicate);
+            var query = _repo.FindByCondition(predicate).Where(e => e.OwnerId == null || e.OwnerId == user.UserId); //.Where(predicate);
             if (additionalQuery != null)
             {
                 query = additionalQuery(query);

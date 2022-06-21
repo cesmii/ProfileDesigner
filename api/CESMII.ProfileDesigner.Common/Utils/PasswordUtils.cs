a520502841faa7e7b31b5c815a002686b9e196ba
scoxen1 <sean.coxen@gmail.com>
2022-06-21 17:21:52 -0400

    more sonar cloud cleanup

--- api/CESMII.ProfileDesigner.Common/Utils/PasswordUtils.cs
+++ api/CESMII.ProfileDesigner.Common/Utils/PasswordUtils.cs
@@ -9,8 +9,7 @@
     public static class PasswordUtils
     {
         private static readonly string _delimiter = "$";
-        private static readonly string _delimiterLegacy = "$";
-
+        
         /// <summary>
         /// Generate a random password with no special characters
         /// </summary>
@@ -103,53 +102,6 @@
 
             //true if they match, existing hash in 3rd position
             return hashed == passwordParts[2];
-        }
-
-        /// <summary>
-        /// Encrypt the legacy password using the DJango approach
-        /// </summary>
-        /// <remarks>
-        /// Reference: https://docs.djangoproject.com/en/3.1/topics/auth/passwords/
-        /// By default, Django uses the PBKDF2 algorithm with a SHA256 hash
-        /// The DB stores the password in this format: <algorithm>$<iterations>$<salt>$<hash>
-        /// Example: "pbkdf2_sha256$216000$r1jfzvUNnesy$HazE155XipRbEWq4JWc2YNCetEH/zYd+H8zaobU1x10="
-        /// </remarks>
-        /// <param name="encoded">
-        /// User's password in the user table as is from the DB (encrypted). It may be in the old format (described in remarks). Or in our converted format. 
-        /// Either way, test if the password passed in matches the encrypted portion of the legacyPassword.
-        /// </param>
-        /// <param name="password">
-        /// The password.
-        /// </param>
-        /// <returns>
-        /// true/false - is there a match using the legacy password conversion
-        /// </returns>
-        public static bool ValidateLegacyPassword(string encoded, string password)
-        {
-            if (string.IsNullOrEmpty(encoded)) return false;
-            if (string.IsNullOrEmpty(password)) return false;
-
-            string[] passwordParts = encoded.Split(_delimiterLegacy);
-            if (passwordParts.Length < 4) return false;
-
-            if (passwordParts[0] != "pbkdf2_sha256") throw new NotSupportedException($"Password - invalid legacy encryption format: {passwordParts[0]}.");
-
-            //by inspecting Django code
-            //force salt to bytes
-            int numBytes = 32;
-            var salt = passwordParts[2];
-            var convertedSalt = System.Text.Encoding.UTF8.GetBytes(salt);
-
-            //perform hash
-            var hash = KeyDerivation.Pbkdf2(
-                password: password,
-                salt: convertedSalt,
-                prf: KeyDerivationPrf.HMACSHA256,
-                iterationCount: Convert.ToInt32(passwordParts[1]),
-                numBytesRequested: numBytes);
-            var hashed = Convert.ToBase64String(hash);
-            //true if they match
-            return hashed == passwordParts[3];
         }
 
     }

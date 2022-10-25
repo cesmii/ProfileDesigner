e038d2e8be58cad05098c89cf3f108f778e01521
Markus Horstmann <markushorstmann@hotmail.com>
2022-10-25 13:18:13 -0700

    CLoudLib tests to separate class

--- api/Tests/CESMII.ProfileDesigner.Api.Tests/CloudLibIntegrationTest.cs
+++ api/Tests/CESMII.ProfileDesigner.Api.Tests/CloudLibIntegrationTest.cs
@@ -6,6 +6,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.IO.Packaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -22,11 +23,21 @@ using Xunit.Sdk;
 
 namespace CESMII.ProfileDesigner.Api.Tests
 {
-    public partial class Integration
+    [TestCaseOrderer("CESMII.ProfileDesigner.Api.Tests.ImportExportIntegrationTestCaseOrderer", "CESMII.ProfileDesigner.Api.Tests")]
+    public class CloudLib : IClassFixture<CustomWebApplicationFactory<CESMII.ProfileDesigner.Api.Startup>>
     {
+        private readonly CustomWebApplicationFactory<CESMII.ProfileDesigner.Api.Startup> _factory;
+        private readonly ITestOutputHelper output;
+
+        public CloudLib(CustomWebApplicationFactory<CESMII.ProfileDesigner.Api.Startup> factory, ITestOutputHelper output)
+        {
+            _factory = factory;
+            this.output = output;
+        }
+    
         [Theory]
         [MemberData(nameof(TestKeywords))]
-        public async Task CloudLibrary(string[] keywords, int expectedCount)
+        public async Task CloudLibrary(string[] keywords, int expectedCount, int expectedNotLocal, int expectedPlusLocal, int expectedNotLocalPlusLocal)
         {
             // Arrange
             var apiClient = _factory.GetApiClientAuthenticated();
@@ -35,22 +46,25 @@ namespace CESMII.ProfileDesigner.Api.Tests
             var allLocalProfiles = (await apiClient.LibraryAsync(new PagerFilterSimpleModel { Query = null, Skip = 0, Take = 100 })).Data;
             var allCloudProfiles = await PagedVsNonPagedAsync(apiClient, new CloudLibFilterModel { Keywords = null, Cursor = null, Take = 100, });
 
-            var cloud = await PagedVsNonPagedAsync(apiClient, new CloudLibFilterModel { Keywords = keywords, Cursor = null, Take = 100,  });
+            var cloud = await PagedVsNonPagedAsync(apiClient, new CloudLibFilterModel { Keywords = keywords, Cursor = null, Take = 100, });
             Assert.Equal(expectedCount, cloud.Count);
+
             var cloudNotLocal = await PagedVsNonPagedAsync(apiClient, new CloudLibFilterModel { Keywords = keywords, Cursor = null, Take = 100, ExcludeLocalLibrary = true, });
+            Assert.Equal(expectedNotLocal, cloudNotLocal.Count);
 
             var findLocals = cloudNotLocal.Where(c => allLocalProfiles.Any(l => l.Namespace == c.Namespace)).ToList();
             Assert.Empty(findLocals);
 
             var cloudPlusLocal = await PagedVsNonPagedAsync(apiClient, new CloudLibFilterModel { Keywords = keywords, Cursor = null, Take = 100, AddLocalLibrary = true, });
-
+            Assert.Equal(expectedPlusLocal, cloudPlusLocal.Count);
 
             var cloudNotLocalPlusLocal = await PagedVsNonPagedAsync(apiClient, new CloudLibFilterModel { Keywords = keywords, Cursor = null, Take = 100, ExcludeLocalLibrary = true, AddLocalLibrary = true });
+            Assert.Equal(expectedNotLocalPlusLocal, cloudNotLocalPlusLocal.Count);
         }
 
         [Theory]
         [MemberData(nameof(TestKeywords))]
-        public async Task CloudLibrarySingle(string[] keywords, int expectedCount)
+        public async Task CloudLibrarySingle(string[] keywords, int expectedCount, int expectedNotLocal, int expectedPlusLocal, int expectedNotLocalPlusLocal)
         {
             // Arrange
             var apiClient = _factory.GetApiClientAuthenticated();
@@ -61,35 +75,52 @@ namespace CESMII.ProfileDesigner.Api.Tests
         }
         [Theory]
         [MemberData(nameof(TestKeywords))]
-        public async Task CloudLibraryPaged(string[] keywords, int expectedCount)
+        public async Task CloudLibraryPaged(string[] keywords, int expectedCount, int expectedNotLocal, int expectedPlusLocal, int expectedNotLocalPlusLocal)
         {
             // Arrange
             var apiClient = _factory.GetApiClientAuthenticated();
 
             // ACT
             var cloudPaged = await GetAllPaged(apiClient, new CloudLibFilterModel { Keywords = keywords, Cursor = null, Take = 5, });
+            Assert.Equal(expectedCount, cloudPaged.Count);
         }
 
         [Theory]
         [MemberData(nameof(TestKeywords))]
-        public async Task CloudLibraryPagedNoLocal(string[] keywords, int _)
+        public async Task CloudLibraryPagedNoLocal(string[] keywords, int expectedCount, int expectedNotLocal, int expectedPlusLocal, int expectedNotLocalPlusLocal)
         {
             // Arrange
             var apiClient = _factory.GetApiClientAuthenticated();
 
             // ACT
             var cloudPaged = await GetAllPaged(apiClient, new CloudLibFilterModel { Keywords = keywords, Cursor = null, Take = 5, ExcludeLocalLibrary = true });
+            Assert.Equal(expectedNotLocal, cloudPaged.Count);
+        }
+        [Theory]
+        [MemberData(nameof(TestKeywords))]
+        public async Task CloudLibraryPagedNoLocalPlusLocal(string[] keywords, int expectedCount, int expectedNotLocal, int expectedPlusLocal, int expectedNotLocalPlusLocal)
+        {
+            // Note: the current results seem incomplete because local profile keyword search only matches the namespace, not the displaynames of hte profile type definitions etc.
+            // Once CloudLib and local keyword search are identical, this should return the same counts as expectedPlusLocal
+
+            // Arrange
+            var apiClient = _factory.GetApiClientAuthenticated();
+
+            // ACT
+            var cloudPaged = await GetAllPaged(apiClient, new CloudLibFilterModel { Keywords = keywords, Cursor = null, Take = 5, AddLocalLibrary = true, ExcludeLocalLibrary = true });
+            Assert.Equal(expectedNotLocalPlusLocal, cloudPaged.Count);
         }
 
         [Theory]
         [MemberData(nameof(TestKeywords))]
-        public async Task CloudLibraryPagedAddLocal(string[] keywords, int _)
+        public async Task CloudLibraryPagedAddLocal(string[] keywords, int expectedCount, int expectedNotLocal, int expectedPlusLocal, int expectedNotLocalPlusLocal)
         {
             // Arrange
             var apiClient = _factory.GetApiClientAuthenticated();
 
             // ACT
             var cloudPaged = await GetAllPaged(apiClient, new CloudLibFilterModel { Keywords = keywords, Cursor = null, Take = 5, AddLocalLibrary = true });
+            Assert.Equal(expectedPlusLocal, cloudPaged.Count);
         }
 
         private async Task<ICollection<CloudLibProfileModel>> PagedVsNonPagedAsync(Client apiClient, CloudLibFilterModel filter)
@@ -134,25 +165,26 @@ namespace CESMII.ProfileDesigner.Api.Tests
 
         public static IEnumerable<object[]> TestKeywords()
         {
-            return new List<object[]> 
+            return new List<object[]>
             {
-                new object[ ]{ null, 54 },
-                new object[] { new string[] { "BaseObjectType" },  6 },
-                new object[] { new string[] { "di" }, 54 },
-                new object[] { new string[] { "robotics" }, 1 },
-                new object[] { new string[] { "plastic" }, 15 },
-                new object[] { new string[] { "pump" } , 6},
-                new object[] { new string[] { "robotics", "di" }, 54 },
-                new object[] { new string[] { "robotics", "di", "pump", "plastic" }, 54 },
-                new object[] { new string[] { "robotics", "pump", }, 7 },
-                new object[] { new string[] { "robotics", "plastic" }, 16 },
-                new object[] { new string[] { "robotics", "pump", "plastic" }, 19 },
-                new object[] { new string[] { "abcdefg", "defghi", "dhjfhsdjfhsdjkfhsdjkf", "dfsjdhfjkshdfjksd" } , 0 },
-                new object[] { new string[] { "Interface" }, 24 },
-                new object[] { new string[] { "Event" }, 22 },
-                new object[] { new string[] { "Interface", "BaseObjectType" }, 28 },
-                new object[] { new string[] { "BaseObjectType", "Interface" }, 28 },
-                new object[] { new string[] { "Interface", "BaseObjectType", "Event" }, 39 },
+                // string[] keywords, int expectedCount, int expectedNotLocal, int expectedPlusLocal, int expectedNotLocalPlusLocal 
+                new object[ ]{ null, 63, 5, 67, 67, },
+                new object[] { new string[] { "BaseObjectType" }, 6, 0, 6, 0, },
+                new object[] { new string[] { "di" }, 61, 3, 61, 11, },
+                new object[] { new string[] { "robotics" }, 2, 1, 2, 2, },
+                new object[] { new string[] { "plastic" }, 15, 0, 15, 14, },
+                new object[] { new string[] { "pump" } , 6, 0, 6, 2,},
+                new object[] { new string[] { "robotics", "di" }, 61, 3, 61, 12, },
+                new object[] { new string[] { "robotics", "di", "pump", "plastic" }, 61, 3, 61, 26, },
+                new object[] { new string[] { "robotics", "pump", }, 8, 1, 8, 4, },
+                new object[] { new string[] { "robotics", "plastic" }, 17, 1, 17, 16, },
+                new object[] { new string[] { "robotics", "pump", "plastic" }, 20, 1, 20, 17, },
+                new object[] { new string[] { "abcdefg", "defghi", "dhjfhsdjfhsdjkfhsdjkf", "dfsjdhfjkshdfjksd" } , 0, 0, 0, 0, },
+                new object[] { new string[] { "Interface" }, 24, 0, 24, 0, },
+                new object[] { new string[] { "Event" }, 23, 1, 23, 1, },
+                new object[] { new string[] { "Interface", "BaseObjectType" }, 28, 0, 28, 0, },
+                new object[] { new string[] { "BaseObjectType", "Interface" }, 28, 0, 28, 0, },
+                new object[] { new string[] { "Interface", "BaseObjectType", "Event" }, 40, 1, 40, 1, },
             };
         }
     }

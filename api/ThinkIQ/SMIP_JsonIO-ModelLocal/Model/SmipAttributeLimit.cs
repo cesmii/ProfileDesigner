dc50e5fb362506fc11a846c398ec5f1243b54d71
Markus Horstmann <markushorstmann@hotmail.com>
2022-12-13 18:34:56 -0800

    Experimental SMIP export

---
+++ api/ThinkIQ/SMIP_JsonIO-ModelLocal/Model/SmipAttributeLimit.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMIP.JsonIO.Model
+{
+    public class SmipAttributeLimit
+    {
+
+        [JsonProperty("direction")]
+        public string Direction { get; set; }
+
+        [JsonProperty("float_value")]
+        public float FloatValue { get; set; }
+
+        public SmipAttributeLimit()
+        {
+            Direction = "";
+        }
+
+    }
+}
---
+++ api/ThinkIQ/SMIP_JsonIO-ModelLocal/Model/SmipQuantity.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMIP.JsonIO.Model
+{
+    public class SmipQuantity
+    {
+
+        [JsonProperty("quantity_symbol")]
+        public string QuantitySymbol { get; set; }
+
+        public SmipQuantity()
+        {
+            QuantitySymbol = "";
+        }
+
+    }
+}

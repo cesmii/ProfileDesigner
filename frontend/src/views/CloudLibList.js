0bdd5983e5ea12e8e33be38d553d2fceb7479711
scoxen1 <sean.coxen@gmail.com>
2022-12-21 12:17:09 -0500

    update import cloud library selected ui.

--- frontend/src/views/CloudLibList.js
+++ frontend/src/views/CloudLibList.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Helmet } from "react-helmet"
 
 import { AppSettings } from '../utils/appsettings'
 import { renderTitleBlock } from '../utils/UtilityService'
-import { useLoadingContext } from "../components/contexts/LoadingContext";
 
 import CloudLibraryImporter from "./shared/CloudLibraryImporter";
 
@@ -11,7 +10,7 @@ import color from '../components/Constants'
 import './styles/ProfileList.scss';
 import '../components/styles/InfoPanel.scss';
 
-const CLASS_NAME = "CloudLibList";
+//const CLASS_NAME = "CloudLibList";
 
 function CloudLibList() {
 
@@ -35,20 +34,6 @@ function CloudLibList() {
         );
     };
 
-    const renderIntroContent = () => {
-        return (
-            <div className="header-actions-row mb-3 pr-0">
-                <p className="mb-2" >
-                    Search the CESMII Cloud Library for Profiles and import Profiles into the Profile Library.
-                </p>
-                <p className="mb-2" >
-                    Type definitions within these Profiles can then be viewed or extended to make new Type definitions, which can become part of one of your SM Profiles.
-                </p>
-            </div>
-        );
-    }
-
-
     //-------------------------------------------------------------------
     // Region: Render final output
     //-------------------------------------------------------------------
@@ -58,7 +43,6 @@ function CloudLibList() {
                 <title>{AppSettings.Titles.Main + " | " + caption}</title>
             </Helmet>
             {renderHeaderRow()}
-            {renderIntroContent()}
             <CloudLibraryImporter/>
         </>
     )

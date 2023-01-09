ec5f1004d4ea1a5c5f15ae3b80a7eca10c313922
Paul Yao <paul@paulyao.com>
2023-01-09 10:22:52 -0500

    Add email address to Admin View Users page

---
+++ sql/CESMII.ProfileDesigner.DB-UPDATE-2023-01-04.sql
@@ -0,0 +1,9 @@
+﻿---------------------------------------------------------------------
+--  Profile Designer DB - Update
+--	Date: 2023-01-04
+--	Who: Paul Y
+--	Details:
+--	Adding email to user list
+---------------------------------------------------------------------
+ALTER TABLE public.user
+    ADD email_address character varying(320) COLLATE pg_catalog."default" NULL;

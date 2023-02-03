35f36e4df40cc16ba13de22d1d8de2b8f3cecdd0
scoxen1 <sean.coxen@gmail.com>
2023-02-03 16:50:16 -0500

    code to make stored function calls against DB and return a tabular result or a count result

--- api/CESMII.ProfileDesigner.Data/Contexts/ProfileDesignerPgContext.cs
+++ api/CESMII.ProfileDesigner.Data/Contexts/ProfileDesignerPgContext.cs
@@ -194,6 +194,10 @@
             {
                 e.HasNoKey();
             });
+            modelBuilder.Entity<StoredProcedureCount>(e =>
+            {
+                e.HasNoKey();
+            });
 
         }
     }

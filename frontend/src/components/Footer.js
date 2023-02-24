f82f3babab2dace5412f7fd0dc7cb12270fb2639
Paul Yao <paul@paulyao.com>
2023-02-24 11:39:53 -0500

    Footer: fixed copyright symbol

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
         <div id="--cesmii-global-footer" className="row m-0 py-4">
             <div className="col-sm-12">
                 <p className="text-center mb-0">
-                    © Copyright 2022-2023, CESMII. All rights reserved.
+                    &copy Copyright 2022-2023, CESMII. All rights reserved.
                 </p>
             </div>
 

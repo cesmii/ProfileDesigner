39f989fafac9fcfa150efd6936d9335e7cebfeb7
Paul Yao <paul@paulyao.com>
2023-05-01 13:53:28 -0400

    Ssse - Add use of IUserSignUpData and UserSignUpModel to complete integration of common support with Profile Designer

---
+++ api/CESMII.ProfileDesigner.DAL/models/UserSignUpData.cs
@@ -0,0 +1,44 @@
+﻿
+namespace CESMII.ProfileDesigner.DAL.Models
+{
+    using CESMII.Common.SelfServiceSignUp.Models;
+    using CESMII.ProfileDesigner.DAL.Models;
+
+    public class UserSignUpData : IUserSignUpData
+    {
+        private readonly UserDAL _dalUser;
+
+        public UserSignUpData(UserDAL dal)
+        {
+            _dalUser = dal;
+        }
+
+        /// <summary>
+        /// Search for user by email address.
+        /// </summary>
+        /// <param name="strEmail"></param>
+        /// <returns></returns>
+        public int Where(string strEmail)
+        {
+            var mylist = _dalUser.Where(x => x.EmailAddress.ToLower().Equals(strEmail.ToLower()), null).Data;
+            return mylist.Count;
+        }
+
+        /// <summary>
+        /// Add user to database.
+        /// </summary>
+        /// <param name="usersum"></param>
+        public async void AddUser(UserSignUpModel usersum)
+        {
+            UserModel um = new UserModel()
+            {
+                DisplayName = usersum.DisplayName,
+                Email = usersum.Email,
+                SelfServiceSignUp_Organization_Name = usersum.Organization,
+                SelfServiceSignUp_IsCesmiiMember = usersum.IsCesmiiMember
+            };
+
+            await _dalUser.AddAsync(um);
+        }
+    }
+}

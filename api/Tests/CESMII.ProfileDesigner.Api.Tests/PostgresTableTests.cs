715d610f2bce6a388fd8dde1c135e7b5303d5b98
Markus Horstmann <markushorstmann@hotmail.com>
2023-07-17 18:39:00 -0700

    Test fix

--- api/Tests/CESMII.ProfileDesigner.Api.Tests/PostgresTableTests.cs
+++ api/Tests/CESMII.ProfileDesigner.Api.Tests/PostgresTableTests.cs
@@ -36,7 +36,7 @@ namespace CESMII.ProfileDesigner.Api.Tests
         /// <param name="strTable"></param>
         /// <param name="expectedCount"></param>
         [Theory]
-        [InlineData("profile", 23)]
+        [InlineData("profile", 28)]
         [InlineData("profile_type_definition", 25)]
         [InlineData("profile_attribute", 44)]
         public void CheckTableColumnCount(string strTable, int expectedCount)
